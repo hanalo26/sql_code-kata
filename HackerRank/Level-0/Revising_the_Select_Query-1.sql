68c6372ac4e97440db354b726ad15c5c9f5548b9
hanalo26 <skyoung100@gmail.com>
2026-06-09 21:13:21 +0900

    260609 SQL 코드카타 풀이 완료

---
+++ HackerRank/Level-0/Revising_the_Select_Query-1.sql
@@ -0,0 +1,7 @@
+-- Revising the Select Query I
+-- 링크: https://www.hackerrank.com/challenges/revising-the-select-query/problem
+-- 작성일: 2026.06.09
+
+SELECT *
+FROM city
+WHERE population >= 100000 AND countrycode = 'USA'
---
+++ HackerRank/Level-0/Revising_the_Select_Query-2.sql
@@ -0,0 +1,7 @@
+-- Revising the Select Query II
+-- 링크: https://www.hackerrank.com/challenges/revising-the-select-query-2/problem
+-- 작성일: 2026.06.09
+
+SELECT name
+FROM city
+WHERE population >= 120000 AND countrycode = 'USA'
