9a78b7fbf6a34f9db2b504b8f775eaf09247a7d1
hanalo26 <skyoung100@gmail.com>
2026-06-10 13:37:56 +0900

    260610 SQL 코드카타

---
+++ HackerRank/Level-0/Select_All.sql
@@ -0,0 +1,6 @@
+-- Select All
+-- 링크: https://www.hackerrank.com/challenges/select-all-sql/problem
+-- 작성일: 2026.06.10 
+
+SELECT *
+FROM city
---
+++ HackerRank/Level-0/Select_By_ID.sql
@@ -0,0 +1,7 @@
+-- Select By ID
+-- 링크: https://www.hackerrank.com/challenges/select-by-id/problem
+-- 작성일: 2026.06.10
+
+SELECT *
+FROM city
+WHERE id = '1661'
