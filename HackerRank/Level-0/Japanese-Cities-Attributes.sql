6eab5bca2eca44a6803484e44c222671f03b0420
hanalo26 <skyoung100@gmail.com>
2026-06-13 15:56:19 +0900

    260613 SQL code-kata

---
+++ HackerRank/Level-0/Japanese-Cities-Attributes.sql
@@ -0,0 +1,7 @@
+-- Japanese Cities' Attributes
+-- 링크: https://www.hackerrank.com/challenges/japanese-cities-attributes/problem
+-- 작성일: 2026.06.13
+
+SELECT *
+FROM city
+WHERE countrycode = 'JPN'
---
+++ HackerRank/Level-0/Japanese-Cities-Names.sql
@@ -0,0 +1,7 @@
+-- Japanese Cities' Names
+-- 링크: https://www.hackerrank.com/challenges/japanese-cities-name/problem
+-- 작성일: 2026.06.13
+
+SELECT name
+FROM city
+WHERE countrycode = 'JPN'
