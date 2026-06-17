1874849d5824f3c97682b94e19017276b49e11da
hanalo26 <skyoung100@gmail.com>
2026-06-17 14:00:23 +0900

    260617 SQL code-kata

---
+++ HackerRank/Level-0/The_Count_Function.sql
@@ -0,0 +1,4 @@
+-- Revising Aggregations - The Count Function
+-- 링크: https://www.hackerrank.com/challenges/revising-aggregations-the-count-function/problem
+-- 작성일: 2026.06.17
+
---
+++ HackerRank/Level-0/The_Sum_Function.sql
@@ -0,0 +1,7 @@
+-- Revising Aggregations - The Sum Function
+-- 링크: https://www.hackerrank.com/challenges/revising-aggregations-sum/problem
+-- 작성일: 2026.06.17
+
+SELECT SUM(population)
+FROM city
+WHERE district = 'California';
