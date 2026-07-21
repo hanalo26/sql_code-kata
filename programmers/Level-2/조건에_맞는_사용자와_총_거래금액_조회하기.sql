444db0d2d1af03d0d3a288e6acc804d3a6d6d310
hanalo26 <skyoung100@gmail.com>
2026-07-21 12:25:32 +0900

    230721 SQL code-kata

---
+++ programmers/Level-2/조건에_맞는_사용자와_총_거래금액_조회하기.sql
@@ -0,0 +1,14 @@
+-- 조건에 맞는 사용자와 총 거래금액 조회하기
+-- 링크:https://school.programmers.co.kr/learn/courses/30/lessons/164668
+-- 작성일: 2026.07.21
+
+SELECT
+	U.USER_ID, 
+	U.NICKNAME,
+	SUM(B.PRICE) AS TOTAL_SALES
+FROM USED_GOODS_BOARD B
+JOIN USED_GOODS_USER U ON B.WRITER_ID = U.USER_ID
+WHERE B.STATUS = "DONE"
+GROUP BY U.USER_ID, U.NICKNAME
+HAVING SUM(B.PRICE) >= 700000
+ORDER BY TOTAL_SALES;
